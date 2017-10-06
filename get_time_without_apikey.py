19d6de8f5be1a091465e543a6ce8289524d258a2
Nishant Agrawal <nishant.102397@gmail.com>
2017-10-06 23:43:07 +0530

    Travel time without using api key

---
+++ get_time_without_apikey.py
@@ -0,0 +1,60 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports
+from urllib import request,parse
+import sys
+import json
+
+# base url to the api
+url = "https://maps.googleapis.com/maps/api/directions/json?"
+
+# block to exit
+def exit_point():
+    e = input("Exit(y/n): ")
+    if e=='Y' or e=='y':
+        print("\nThanks for using this service. HAPPY TRAVELLING.\n")
+        sys.exit()
+    elif e=='n' or e=='N':
+        main()
+    else:
+        print("Invalid option")
+        exit_point()
+
+
+def travel_time(frm,to,mode):
+    try:
+        new_url = url + parse.urlencode({'origin':frm,'destination':to,'mode':mode})
+        data = request.urlopen(new_url).read().decode()
+        try:
+            js = json.loads(data)
+        except:
+            js = None
+        if not js or 'status' not in js or js['status']!='OK':
+            print('==== Failure To Retrieve ====')
+            exit_point()
+        return js["routes"][0]["legs"][0]["duration"]["text"]
+    except:
+        print("Check your network connection")
+        
+
+
+def main():
+    frm = input("Origin : ").lower();
+    if len(frm)<1:
+        exit_point()
+    to = input("Destination : ").lower();
+    if len(to)<1:
+        exit_point()
+    mode = input("Mode(Driving,Train,Walking,Bicycle) : ").lower();
+    while mode not in ["driving","train","walking","bicycle"]:
+        if len(mode)<1:
+            exit_point()
+        print("Choose from given options only")
+        mode = input("Mode(Driving,Train,Walking,Bicycle) : ").lower();
+    netTime = travel_time(frm,to,mode)
+    print("\nIt will take %s to travel from %s to %s." %(netTime, frm, to))
+    exit_point()
+
+if __name__ =='__main__':
+    main()
